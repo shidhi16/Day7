22d94b4a636ba3c9bad4f9df250ad27a3f21341c
Shivani <shivanidhiman1619@gmail.com>
2019-06-14 18:16:59 -0400

    Initial Commit

---
+++ Day7/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Day7
+//
+//  Created by Vijender Singh on 2019-06-14.
+//  Copyright © 2019 Shivani Dhiman. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
